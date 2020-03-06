83935d2d8a4d980a2b59439c0f2bc0c07e94a63f
HyunJun <shj1504@korea.ac.kr>
2020-03-06 17:22:33 +0900

    Unit test, Lombok

---
+++ src/main/java/com/getmyhome/springboot/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.getmyhome.springboot.web;
+
+import com.getmyhome.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello springframework!!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}

1c517e2388453fc7c046af64f67b0596c06d7714
hyushin <hyushin@student.42seoul.kr>
2020-03-06 23:54:41 +0900

    change config

--- src/main/java/com/getmyhome/springboot/web/HelloController.java
+++ src/main/java/com/getmyhome/springboot/web/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name,
-                                     @RequestParam("amount") int amount) {
+                                     @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
     }
 }

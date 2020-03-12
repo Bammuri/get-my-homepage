df56cbf9125dede14a5fc505cd1e134953b172a0
hyushin <hyushin@student.42seoul.kr>
2020-03-12 22:14:36 +0900

    make CRUD function

--- src/main/java/com/getmyhome/springboot/service/posts/PostsService.java
+++ src/main/java/com/getmyhome/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.getmyhome.springboot.service.posts;
 
 import com.getmyhome.springboot.domain.posts.Posts;
 import com.getmyhome.springboot.domain.posts.PostsRepository;
+import com.getmyhome.springboot.web.dto.PostsListResponseDto;
 import com.getmyhome.springboot.web.dto.PostsResponseDto;
 import com.getmyhome.springboot.web.dto.PostsSaveRequestDto;
 import com.getmyhome.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,11 +33,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
+    }
+
     @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/getmyhome/springboot/web/PostsApiController.java
+++ src/main/java/com/getmyhome/springboot/web/PostsApiController.java
@@ -23,6 +23,12 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
         return postsService.findById(id);

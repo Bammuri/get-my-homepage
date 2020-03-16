c1a8dfde727f254400879c635a21036126afe011
hyushin <hyushin@student.42seoul.kr>
2020-03-16 22:20:18 +0900

    complete google login

--- src/main/java/com/getmyhome/springboot/web/IndexController.java
+++ src/main/java/com/getmyhome/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.getmyhome.springboot.web;
 
+import com.getmyhome.springboot.config.auth.dto.SessionUser;
 import com.getmyhome.springboot.service.posts.PostsService;
 import com.getmyhome.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
         private final PostsService postsService;
+        private final HttpSession httpSession;
 
         @GetMapping("/")
         public String index(Model model) {
             model.addAttribute("posts", postsService.findAllDesc());
+            SessionUser user = (SessionUser) httpSession.getAttribute("user");
+            if (user != null) {
+                model.addAttribute("userName", user.getName());
+            }
             return "index";
         }
 

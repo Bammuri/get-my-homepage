df7ef689dc6cecf2aa9399e8c8aede19b147e4e0
hyushin <hyushin@student.42seoul.kr>
2020-03-17 22:06:19 +0900

    refactoring for getting session info, using database for session info saving, adding NAVER login

--- src/main/java/com/getmyhome/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/getmyhome/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,8 +27,12 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
+
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
@@ -39,6 +43,19 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
 
     public User toEntity() {
         return User.builder()
--- src/main/java/com/getmyhome/springboot/web/IndexController.java
+++ src/main/java/com/getmyhome/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.getmyhome.springboot.web;
 
+import com.getmyhome.springboot.config.auth.LoginUser;
 import com.getmyhome.springboot.config.auth.dto.SessionUser;
 import com.getmyhome.springboot.service.posts.PostsService;
 import com.getmyhome.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
         private final HttpSession httpSession;
 
         @GetMapping("/")
-        public String index(Model model) {
+        public String index(Model model, @LoginUser SessionUser user) {
             model.addAttribute("posts", postsService.findAllDesc());
-            SessionUser user = (SessionUser) httpSession.getAttribute("user");
             if (user != null) {
                 model.addAttribute("userName", user.getName());
             }

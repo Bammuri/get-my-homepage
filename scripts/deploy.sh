12dc2e13ab461aae6618ce07c9f18f305d2994ca
hyushin <hyushin@student.42seoul.kr>
2020-03-25 15:11:08 +0900

    modify zip file name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,10 +11,10 @@ echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl getmyhome | grep jar | awk '{print $1}')
 
-echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구종중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         echo "> kill -15  $CURRENT_PID"
         kill -15 $CURRENT_PID

23774c829eb48dfab9126259c1c109079c48f32e
hyushin <hyushin@student.42seoul.kr>
2020-03-25 18:41:16 +0900

    test modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application-real.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-oauth.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &

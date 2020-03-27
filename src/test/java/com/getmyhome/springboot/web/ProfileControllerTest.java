fb521ed5591eace07dec427851cef3cd57163509
hyushin <hyushin@student.42seoul.kr>
2020-03-27 15:39:55 +0900

    modify failing build test to ProfileContriollerTest

--- src/test/java/com/getmyhome/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/getmyhome/springboot/web/ProfileControllerTest.java
@@ -1,14 +1,19 @@
 package com.getmyhome.springboot.web;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
 
     @LocalServerPort
